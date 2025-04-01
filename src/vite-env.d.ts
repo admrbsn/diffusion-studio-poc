ac37fcc81e0cc37a7036a5d40520dc257d925bbf
admrbsn <admrbsn@gmail.com>
2025-04-01 11:28:02 -0600

    adding vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  esbuild: {
+    target: 'esnext', // enables top-level await support
+  },
+})
