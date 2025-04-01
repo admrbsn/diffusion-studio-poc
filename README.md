2f61db9108cfbe42e89d33e56a7076871bfb5d1b
admrbsn <admrbsn@gmail.com>
2025-04-01 11:16:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# diffusion-studio-poc
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/admrbsn/diffusion-studio-poc)
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css';
+import { setupControls } from './controls';
+import { setupTimeline } from './timeline';
+
+import { composition } from './compositions/example';
+
+// connect to ui
+setupControls(composition);
+setupTimeline(composition);
